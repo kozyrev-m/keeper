2edcdf7bdf8aff6a0c143b0830b294ab3724040c
mikhailkozyrev1 <mikhail1000@mail.ru>
2023-01-28 15:54:50 +0300

    increment2: add new migration

---
+++ migrations/20230126211740_keeper.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    login VARCHAR UNIQUE,
+    encrypted_password VARCHAR
+);
+CREATE TABLE private_data (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    owner_id INT NOT NULL,
+    type_id INT NOT NULL UNIQUE,
+    metadata VARCHAR NOT NULL,
+    content VARCHAR NOT NULL,
+    created_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+DROP TABLE private_data;
+-- +goose StatementEnd
