e1e28eec1c12fe1185f8f2908a1866e441eeaf93
mikhailkozyrev1 <mikhail1000@mail.ru>
2023-02-01 22:12:14 +0300

    increment4: add model for login password pair

---
+++ internal/master/model/datamodel/loginpassword.go
@@ -0,0 +1,47 @@
+package datamodel
+
+import "strings"
+
+const (
+	separator = "&&SEP&&"
+)
+
+// LoginPassword contains login password pair.
+type LoginPassword struct {
+	BasePart
+	Login    string
+	Password string
+}
+
+// Encrypt encrypts content.
+func (lp *LoginPassword) Encrypt() error {
+
+	value := strings.Join(
+		[]string{lp.Login, lp.Password},
+		separator,
+	)
+
+	enc, err := encrypt(password, value)
+	if err != nil {
+		return err
+	}
+
+	lp.SetEncryptedContent(enc)
+
+	return nil
+}
+
+// Decrypt decrypts content.
+func (lp *LoginPassword) Decrypt(enc string) error {
+	value, err := decrypt(password, enc)
+	if err != nil {
+		return err
+	}
+
+	loginpassword := strings.Split(value, separator)
+
+	lp.Login = loginpassword[0]
+	lp.Password = loginpassword[1]
+
+	return nil
+}
