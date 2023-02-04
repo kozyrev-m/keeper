cc542c9ccf59be6262476888e0993f673520bb10
mikhailkozyrev1 <mikhail1000@mail.ru>
2023-02-04 13:43:28 +0300

    increment5: remove terminal

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"log"
-	"github.com/kozyrev-m/keeper/internal/agent/terminal"
 )
 
 func main() {
-	if err := terminal.NewTerminal().StartTerminal(); err != nil {
-		log.Fatal(err)
-	}
+	log.Println("Starting client...")
 }
--- internal/agent/terminal/constants.go
+++
@@ -1,6 +0,0 @@
-package terminal
-
-const (
-	notauth = "(R) register new user \n(S) login \n(Q) quit"
-	auth = "(R) register new user \n(S) login \n(W) whoami \n(Q) to quit"
-)
